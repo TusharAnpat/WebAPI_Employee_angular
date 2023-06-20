17ec848217ee47a97e79d34945d28aad34957e7e
TusharAnpat <tusharanpat7777@gmail.com>
2023-06-20 19:29:16 +0530

    employee project Web API

---
+++ FullStack API/FullStack.API/FullStack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,58 @@
+﻿using FullStack.API.Data;
+using FullStack.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+        public EmployeesController(FullStackDbContext fullStackDbContext)
+        {
+            _fullStackDbContext = fullStackDbContext;
+        }
+        [HttpGet]
+
+        public async Task<IActionResult> GelAllEmployees()
+        {
+            //fetch all employee list.
+
+            var employees=await _fullStackDbContext.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id= Guid.NewGuid();
+
+            //add new employee 
+            await _fullStackDbContext.Employees.AddAsync(employeeRequest);
+            await _fullStackDbContext.SaveChangesAsync();
+
+            return Ok(employeeRequest);
+        }
+
+        [HttpGet]
+
+        [Route("{id:GUid}")]
+
+        public async Task<IActionResult> GetEmployee([FromRoute] Guid id)
+        {
+           var employee = 
+                await _fullStackDbContext.Employees.FirstOrDefaultAsync(x=>x.Id==id);
+
+            if (employee==null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+
+        }
+
+    }
+}
---
+++ FullStack API/FullStack.API/FullStack.API/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace FullStack.API.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public long phone { get; set; }
+        public long salary { get; set; }
+        public string Department { get; set; }
+    }
+}
